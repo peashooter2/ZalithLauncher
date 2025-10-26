0717aab467b33b5b2b194aafe1d2792438ad251c
aaaapai <89151134+aaaapai@users.noreply.github.com>
2025-10-26 10:27:25 +0800

    Update GLFW.java

--- jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
+++ jre_lwjgl3glfw/src/main/java/org/lwjgl/glfw/GLFW.java
@@ -1178,6 +1178,9 @@ public class GLFW
     }
 
     public static int glfwGetKey(@NativeType("GLFWwindow *") long window, int key) {
+		// This is jank, anything asking for int 348 results in an IndexOutOfBounds because idk.
+        // Probably an off-by-one error. This is the 'fix'
+        if (key == GLFW_KEY_LAST){return GLFW_KEY_LAST;}
         return keyDownBuffer.get(Math.max(0, key-31));
     }
 
